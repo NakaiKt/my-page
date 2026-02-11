2a188c9ccd75df0aba855fbc853a39055e95d62d
KatsuhiroNakai <49671847+NakaiKt@users.noreply.github.com>
2026-02-11 16:42:53 +0900

    Update job title formatting in userMeta

--- src/lib/constants/userMeta.ts
+++ src/lib/constants/userMeta.ts
@@ -1,6 +1,6 @@
 export const USER_META = {
   name: "Katsuhiro Nakai",
-  job: "エンジニア・デザイナー",
+  job: "エンジニア（デザイナー）",
   email: "k.nakai.work@gmail.com",
   avater:
     "https://katsuhiro-nakai-my-page.s3.ap-northeast-1.amazonaws.com/profile/partyparrot.png",
